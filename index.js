fb0241a333df13ed73d0e19d5811c068bd0b573e
Boo-coder <Bouna112@yahoo.com>
2024-11-19 21:37:23 -0500

    scopeLab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+ 
+function upperCaseCustomerName() {
+    customerName = customerName.toLocaleUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'haha!';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'done';
+}
